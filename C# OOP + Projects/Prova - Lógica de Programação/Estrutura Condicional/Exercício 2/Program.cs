2c7743f7076af8c6482aca62ebd9f68bfda775b7
Caio <caiowey@hotmail.com.br>
2021-10-07 17:04:47 -0300

    Fazendo mais exercícios de C#

---
+++ C# OOP + Projects/Prova - Lógica de Programação/Estrutura Condicional/Exercício 2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ler os valores dos três coeficientes "a", "b" e "c" de uma equação do segundo grau. Em seguida, mostrar
+// os valores das raízes da equação, conforme exemplos, usando a fórmula de Baskara. Se a equação não possuir
+// raízes (o valor de "a" não pode ser zero, e o valor de "delta" não pode ser negativo), mostrar uma 
+// mensagem "Impossível calcular".
+
+using System;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
