c3a10e410891ef7a89daf7c953eb2e331880064a
Caio <caiowey@hotmail.com.br>
2021-10-07 14:19:27 -0300

    REFramework practice

---
+++ C# OOP + Projects/Prova - Lógica de Programação/Estrutura Sequencial/Exercício 1.cs
@@ -0,0 +1,12 @@
+// Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código
+// de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+
+using System;
+class Program {
+    static void Main() {
+        Console.WriteLine("Digite o código da peça 1: ");
+        int codPeca1 = Console.ReadLine();
+        Console.WriteLine("Digite o número de peças 1: ");
+        int numPeca1 = Console.ReadLine();
+    }
+}
