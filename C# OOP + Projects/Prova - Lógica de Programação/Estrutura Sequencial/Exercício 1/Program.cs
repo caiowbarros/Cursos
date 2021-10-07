9c73f2abf902cbec4db3dfba4e33094b3e4bca34
Caio <caiowey@hotmail.com.br>
2021-10-07 16:31:14 -0300

    Começando curso de C#

---
+++ C# OOP + Projects/Prova - Lógica de Programação/Estrutura Sequencial/Exercício 1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1,
+// o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+
+using System;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Peça 1
+            Console.WriteLine("Insira o código da peça 1: ");
+            int codPeca1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o número de peças 1:");
+            int numPeca1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o valor unitário de cada peça 1: ");
+            double precoPeca1 = Convert.ToDouble(Console.ReadLine());
+
+            // Peça 2
+            Console.WriteLine("Insira o código da peça 2: ");
+            int codPeca2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o número de peças 2:");
+            int numPeca2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o valor unitário de cada peça 2: ");
+            double precoPeca2 = Convert.ToDouble(Console.ReadLine());
+
+            // Cálculo
+            double total = (numPeca1 * precoPeca1) + (numPeca2 * precoPeca2);
+            Console.WriteLine("VALOR A PAGAR: R$" + Math.Round(total, 2));
+        }
+    }
+}
---
+++ C# OOP + Projects/Prova - Lógica de Programação/Estrutura Sequencial/Exercício 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+// casas decimais conforme exemplos.
+// 
+// Fórmula: área = pi * (raio * raio)
+//
+// Considere o valor de pi = 3.14159
+
+using System;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            // Raio
+            Console.Write("Insira o valor do raio: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            // Cálculo
+            double area = pi * (raio * raio);
+
+            Console.WriteLine("A = " + Math.Round(area, 4));
+        }
+    }
+}
