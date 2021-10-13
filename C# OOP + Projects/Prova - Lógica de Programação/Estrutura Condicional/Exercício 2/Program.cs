41134ee4142fd57b047cf86419a23ba94883b002
Caio <caiowey@hotmail.com.br>
2021-10-13 12:59:20 -0300

    terminando prova de lógica de programação

--- C# OOP + Projects/Prova - Lógica de Programação/Estrutura Condicional/Exercício 2/Program.cs
+++ C# OOP + Projects/Prova - Lógica de Programação/Estrutura Condicional/Exercício 2/Program.cs
@@ -11,7 +11,26 @@ namespace Exercício_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Pegando valores 
+            Console.WriteLine("Digite o valor de a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            // Calculando Baskara
+            double delta = (b * b) - (4 * a * c);
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            // Verificando validez
+            if (a == 0 || delta < 0) {
+                Console.WriteLine("Impossível calcular");
+            } else {
+                Console.WriteLine("X1 = " + x1);
+                Console.WriteLine("X2 = " + x2);
+            }
         }
     }
 }
