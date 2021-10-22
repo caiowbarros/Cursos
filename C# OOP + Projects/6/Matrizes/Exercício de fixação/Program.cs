91705792a52eef0a2dd0fa5320228dab229c4ad9
Caio <caiowey@hotmail.com.br>
2021-10-22 10:17:27 -0300

    Fazendo exercícios de C#

---
+++ C# OOP + Projects/6/Matrizes/Exercício de fixação/Program.cs
@@ -0,0 +1,18 @@
+﻿// Fazer um programa para ler dois números inteiros M e N, e depois ler
+// uma matriz de M linhas por N colunas contendo números inteiros, podendo
+// haver repetições. Em seguida, ler um número inteiro X que pertence à
+// matriz. Para cada ocorrência de X, mostrar os valores à esquerda, acima
+// à direita e abaixo de X, quando houver.
+
+using System;
+
+namespace Exercício_de_fixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
